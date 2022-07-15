9c7c08f7167ed6d8a6598f0ac88168c87c4919e6
Vladislav Bondarchuk <vbondarchuk@rtc.usetech.ru>
2022-07-15 16:17:44 +0300

    update

--- kind/install_kind_linux.sh
+++ kind/install_kind_linux.sh
@@ -1,8 +1,8 @@
 # https://kind.sigs.k8s.io/docs/user/quick-start
 
-KIND_LOCATION=/home/vlad/.local/bin/
+KIND_LOCATION=/home/vbondarchuk/.local/bin/
 
-curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.6.1/kind-$(uname)-amd64
+curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.14.0/kind-$(uname)-amd64
 chmod +x ./kind
 mkdir --parents $KIND_LOCATION
 mv ./kind $KIND_LOCATION
