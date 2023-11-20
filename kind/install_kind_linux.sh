d60d062daa439940319253b1803dd2a9e56eb2f1
Vladislav Bondarchuk <vbondarchuk@rtc.usetech.ru>
2023-11-20 15:15:21 +0300

    update

--- kind/install_kind_linux.sh
+++ kind/install_kind_linux.sh
@@ -2,7 +2,7 @@
 
 KIND_LOCATION=/home/vbondarchuk/.local/bin/
 
-curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.14.0/kind-$(uname)-amd64
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 chmod +x ./kind
 mkdir --parents $KIND_LOCATION
 mv ./kind $KIND_LOCATION
