df13acf83d6466c9403acbba004802345a808627
Vladislav Bondarchuk <vbondarchuk@rtc.usetech.ru>
2023-11-20 15:23:53 +0300

    update

--- kind/install_kubectl_linux.sh
+++ kind/install_kubectl_linux.sh
@@ -1,8 +1,8 @@
-KUBECTL_LOCATION=/home/vlad/.local/bin/
+KUBECTL_LOCATION=~/.local/bin/
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 mkdir --parents $KUBECTL_LOCATION
 mv ./kubectl $KUBECTL_LOCATION
-
+source ~/.profile
 kubectl version
