316b0d73fa8790a73e2dc332650e5e5aacd94d21
Vladislav Bondarchuk <vbondarchuk@rtc.usetech.ru>
2023-11-20 15:18:19 +0300

    update

--- kind/install_kind_linux.sh
+++ kind/install_kind_linux.sh
@@ -1,6 +1,6 @@
 # https://kind.sigs.k8s.io/docs/user/quick-start
 
-KIND_LOCATION=/home/vbondarchuk/.local/bin/
+KIND_LOCATION=~/.local/bin/
 
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 chmod +x ./kind
